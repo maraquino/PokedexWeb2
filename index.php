5659fbcd988e7608a1e40fb45cdbe5c597a4c1f5
Mariana Aquino <maquino501@alumno.unlam.edu.ar>
2023-05-08 19:20:06 -0300

    index

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Pokedex</title>
+</head>
+<body>
+<header>
+    <h1>Pokedex</h1>
+    <form>
+        <input type=text name="ususario" placeholder="usuario">
+        <input type=text name="password" placeholder="contraseña">
+        <input type="submit" name="" value="Ingresar">
+    </form>
+</header>
+
+<section>
+
+</section>
+
+</body>
+</html>
+<?php
